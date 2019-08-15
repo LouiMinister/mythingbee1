01757848dd3bb141aece847144bca1bdefb66db1
dongjaeLee <dongjae0912@naver.com>
2019-08-15 13:56:05 +0900

    [ISSUE-113] 뉴스 크롤링 예제코드 만들기, 라이브러리 추가

--- src/main/java/com/crawlingSample/sample1.java
+++ src/main/java/com/crawlingSample/sample1.java
@@ -5,24 +5,43 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
+
 public class sample1 {
     public static void main(String[] args) throws Exception{
-        String articleURL= "https://news.v.daum.net/v/20190815090039789";   //뉴스기사 URL
-        Document doc = Jsoup.connect(articleURL).get();     // document 객체 생성
-        Elements ele = null;
 
-        String title = doc.select("h3.tit_view").text();
-        String summary = doc.select("strong.summary_view").text();
-        String summary2 = doc.select("div.layer_summary").text();
-        String imgUrl = doc.select("img.thumb_g_article").attr("src");
-
-
-       // String summary3 = doc.getElementsByTag("div.layer_summary").attr("src");
+        String []articleURL= {"https://news.v.daum.net/v/20190815090039789",
+        "https://news.v.daum.net/v/20190814212855807",
+                "https://news.v.daum.net/v/20190815090643916",
+                "https://news.v.daum.net/v/20190812200137883"
+        };   //뉴스기사 URL
+        Document doc =null;
+        for(String s: articleURL) {
+            doc = Jsoup.connect(s).get();     // document 객체 생성
 
 
-        System.out.println(title);
-        System.out.println(summary);
-        System.out.println(summary2);
-        System.out.println(imgUrl);
+            String title = doc.select("h3.tit_view").text();
+            String summary = doc.select("strong.summary_view").text();
+            String summary2 = doc.select("div.layer_summary > *:not(.ico_info_summary)").text();
+            String summary3= doc.select("p[dmcf-ptype=general]").text();
+            String summary4="";
+            if(doc.select("p[dmcf-ptype=general]").first()!=null){
+                summary4= doc.select("p[dmcf-ptype=general]").first().text();
+            }
+            String imgUrl = doc.select("img.thumb_g_article").attr("src");
+            String press = doc.select("meta[name=article:media_name]").attr("content");
+            //press=doc.getElementsByTag("meta").attr("content");
+
+            // String summary3 = doc.getElementsByTag("div.layer_summary").attr("src");
+            System.out.println("TRUEFALSE"+summary.equals(""));
+            System.out.println("제목:"+title);
+            System.out.println("요약"+summary);
+            System.out.println("요약2"+summary2);
+            System.out.println("요약3"+summary3);
+            System.out.println("요약4"+summary4);
+            System.out.println("이미지"+imgUrl);
+            System.out.println("기사"+press);
+            System.out.println("------------------------------------------------------------------");
+        }
     }
 }
