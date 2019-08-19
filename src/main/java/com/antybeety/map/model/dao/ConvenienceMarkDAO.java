0b5ad3c65d40ecbda78a24442be3baf7aa9be239
abty <taehyun16@naver.com>
2019-08-19 09:23:19 +0900

    [ISSUE-220] 모바일 시설물 표시 GET 방식으로 변경

--- src/main/java/com/antybeety/map/model/dao/ConvenienceMarkDAO.java
+++ src/main/java/com/antybeety/map/model/dao/ConvenienceMarkDAO.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Map;
 
-@Repository
+//@Repository
 public class ConvenienceMarkDAO extends FacilityMarkDAOImpl{
 
     @Autowired
